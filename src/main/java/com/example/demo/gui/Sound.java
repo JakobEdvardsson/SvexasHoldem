aa912115481f8e66554bf6cc2cb53631371c58cc
JakobEdvardsson <jakob.edvardsson@outlook.com>
2024-01-24 12:19:31 +0100

    Fixed incorrect audio filename

--- src/main/java/com/example/demo/gui/Sound.java
+++ src/main/java/com/example/demo/gui/Sound.java
@@ -14,15 +14,15 @@ public class Sound {
 
 	private static final String SOUND_BASE_PATH = "/com/example/demo/sounds/";
 
-	private static Media m = new Media(Sound.class.getResource(SOUND_BASE_PATH + "cool_struttin'.mp3").toString());
+	private static Media m = new Media(Sound.class.getResource(SOUND_BASE_PATH + "cool_struttin.mp3").toString());
 	public static MediaPlayer mp = new MediaPlayer(m);
 	public AudioClip checkSound = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "checkMeSound.m4a").toString());
 	public AudioClip shuffleSound = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "cardShuffle.wav").toString());
 	public AudioClip singleCard = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "cardSlide8.wav").toString());
 	public AudioClip cardFold = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "cardShove3.wav").toString());
 	public AudioClip chipSingle = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "chipsStacksSingle.wav").toString());
-	public AudioClip chipMulti = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "chipMe.m4a").toString());
-	public AudioClip coinSound = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "chingChingChip.m4a").toString());
+	public AudioClip chipMulti = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "ChipMe.m4a").toString());
+	public AudioClip coinSound = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "ChingChingChip.m4a").toString());
 	public AudioClip wrongSound = new AudioClip(Sound.class.getResource(SOUND_BASE_PATH + "buttonSoundWrong.mp3").toString());
 
 
