a2524ea5ae24f62bd64594bbeca2ef9913dee17a
Filip Nordqvist <114819894+Dautabossy@users.noreply.github.com>
2024-01-31 15:59:18 +0100

    Added test cases

--- src/test/java/TestSPController.java
+++ src/test/java/TestSPController.java
@@ -5,15 +5,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestSPController {
 
-    @Test
-    public void testSPController() {
+    private static String lower_consonants = "abcdefghijklmnopqrstuvwxzåäö";
+    private static String upper_consonants = "ABCDEFGHIJKLMNOPQRSTUVWXZÅÄÖ";
         SPController SPController = new SPController();
+
+    @Test
+    public void testBetSize() {
         assertEquals(0, SPController.getCurrentMaxBet()); //TODO: Ändra expected value.
         assertEquals(0, SPController.getSmallBlind()); //TODO: Ändra expected value.
     }
 
     @Test
     public void testUserName() {
+        SPController.setName("Username");
+        assertEquals("Username", SPController.getName());
+    }
+
+    @Test
+    public void testUserNameEmpty() {
+        SPController.setName("");
+        assertEquals("", SPController.getName());
+    }
+
+    @Test
+    public void testUserNameNull() {}
+
+    @Test
+    public void testUserNameTooLong() {
 
     }
 }
