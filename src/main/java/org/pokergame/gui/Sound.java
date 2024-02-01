8b15dec77ba38cae6b229d14cffbe3708442dc0b
JakobEdvardsson <jakob.edvardsson@outlook.com>
2024-02-01 23:17:18 +0100

    Fix audio path #22

--- src/main/java/org/pokergame/gui/Sound.java
+++ src/main/java/org/pokergame/gui/Sound.java
@@ -13,44 +13,45 @@ import org.pokergame.utils.PathUtil;
  *
  */
 public class Sound {
-	private static final PathUtil pathUtil = new PathUtil();
+  private static final PathUtil pathUtil = new PathUtil();
 
-	private static final Media m = new Media(pathUtil.getImage("cool_struttin.mp3"));
+  private static final Media m = new Media(pathUtil.getSound("cool_struttin.mp3"));
 
-	public static MediaPlayer mp = new MediaPlayer(m);
-	public AudioClip checkSound = new AudioClip(pathUtil.getSound("checkMeSound.m4a"));
-	public AudioClip shuffleSound = new AudioClip(pathUtil.getSound("cardShuffle.wav"));
-	public AudioClip singleCard = new AudioClip(pathUtil.getSound("cardSlide8.wav"));
-	public AudioClip cardFold = new AudioClip(pathUtil.getSound("cardSlide8.wav"));
-	public AudioClip chipSingle = new AudioClip(pathUtil.getSound("chipsStacksSingle.wav"));
-	public AudioClip chipMulti = new AudioClip(pathUtil.getSound("ChipMe.m4a"));
-	public AudioClip coinSound = new AudioClip(pathUtil.getSound("ChingChingChip.m4a"));
-	public AudioClip wrongSound = new AudioClip(pathUtil.getSound("buttonSoundWrong.mp3"));
+  public static MediaPlayer mp = new MediaPlayer(m);
+  public AudioClip checkSound = new AudioClip(pathUtil.getSound("checkMeSound.m4a"));
+  public AudioClip shuffleSound = new AudioClip(pathUtil.getSound("cardShuffle.wav"));
+  public AudioClip singleCard = new AudioClip(pathUtil.getSound("cardSlide8.wav"));
+  public AudioClip cardFold = new AudioClip(pathUtil.getSound("cardSlide8.wav"));
+  public AudioClip chipSingle = new AudioClip(pathUtil.getSound("chipsStacksSingle.wav"));
+  public AudioClip chipMulti = new AudioClip(pathUtil.getSound("ChipMe.m4a"));
+  public AudioClip coinSound = new AudioClip(pathUtil.getSound("ChingChingChip.m4a"));
+  public AudioClip wrongSound = new AudioClip(pathUtil.getSound("buttonSoundWrong.mp3"));
 
+  /**
+   * Plays the AudioClip.
+   * 
+   * @param type Name of sound that is being sent from the different classes that
+   *             uses the audio objects.
+   */
+  public void playSound(String type) {
+    switch (type) {
+      case "check" -> checkSound.play();
+      case "fold" -> cardFold.play();
+      case "shuffle" -> shuffleSound.play();
+      case "singleCard" -> singleCard.play();
+      case "chipSingle" -> chipSingle.play();
+      case "chipMulti" -> chipMulti.play();
+      case "coinSound" -> coinSound.play();
+      case "wrong" -> wrongSound.play();
+    }
+  }
 
-	/**
-	 * Plays the AudioClip.
-	 * @param type Name of sound that is being sent from the different classes that uses the audio objects.
-	 */
-	public void playSound(String type) {
-        switch (type) {
-            case "check" -> checkSound.play();
-            case "fold" -> cardFold.play();
-            case "shuffle" -> shuffleSound.play();
-            case "singleCard" -> singleCard.play();
-            case "chipSingle" -> chipSingle.play();
-            case "chipMulti" -> chipMulti.play();
-            case "coinSound" -> coinSound.play();
-            case "wrong" -> wrongSound.play();
-        }
-	}
+  /**
+   * Starts playing the background music.
+   */
+  public void playBackgroundMusic() {
+    // mp.play();
 
-	/**
-	 * Starts playing the background music.
-	 */
-	public void playBackgroundMusic() {
-		//mp.play();
-
-	}
+  }
 
 }
