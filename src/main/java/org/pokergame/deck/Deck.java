da9e8b2ce459c3235b2d46bcb75bbb3b6aa743d7
JakobEdvardsson <jakob.edvardsson@outlook.com>
2024-02-07 17:05:29 +0100

    Update Deck.java

--- src/main/java/org/pokergame/deck/Deck.java
+++ src/main/java/org/pokergame/deck/Deck.java
@@ -9,59 +9,49 @@ import java.util.Collections;
 
 /**
  * A deck of cards
- * 
+ *
  * @author Vedrana Zeba
  */
 public class Deck {
   private ArrayList<Card> deck = new ArrayList<Card>();
-  private ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
-
-  /**
-   * Creates a deck of cards
-   */
+  /** Creates a deck of cards */
   public Deck() {
     createDeck();
   }
 
-
-  /**
-   * Shuffles the deck
-   */
+  /** Shuffles the deck */
   public void shuffle() {
     Collections.shuffle(deck);
   }
 
-
   /**
    * Returns the reference of the card that's being removed
-   * 
+   *
    * @return the reference of the card that's being removed
    */
   public Card getCard() {
     return deck.remove(0);
   }
 
-
   /**
    * Returns the current size of the deck
-   * 
+   *
    * @return the current size of the deck
    */
   public int getNumberOfCardsInDeck() {
     return deck.size();
   }
 
-  private static final String BASE_PATH = "/org/pokergame/";
-  /**
-   * Creates a deck of cards
-   */
+  /** Creates a deck of cards */
   public void createDeck() {
     deck = new ArrayList<Card>();
 
     for (Suit suit : Suit.values()) {
       for (CardValue card : CardValue.values()) {
-        ImageIcon cardImageView = new ImageIcon(new PathUtil().getImage(card.getCardValue() + "" + suit.getSuitLetter() + ".png"));
+        ImageIcon cardImageView =
+            new ImageIcon(
+                new PathUtil().getImage(card.getCardValue() + "" + suit.getSuitLetter() + ".png"));
         deck.add(new Card(suit, card, cardImageView));
       }
     }
