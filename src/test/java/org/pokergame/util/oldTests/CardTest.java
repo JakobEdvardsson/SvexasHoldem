f8b373dee30af42acfc2cdbeaa5e3c01f7ed0f14
TabascoTwan <antonlokesandell@gmail.com>
2024-02-09 12:44:52 +0100

    Moved old tests

--- src/test/java/org/pokergame/util/oldTests/CardTest.java
+++ src/test/java/org/pokergame/util/oldTests/CardTest.java
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.pokergame.util;
+package org.pokergame.util.oldTests;
 
 import org.pokergame.Card;
 import org.junit.jupiter.api.Test;
--- src/test/java/org/pokergame/util/oldTests/PokerUtilsTest.java
+++ src/test/java/org/pokergame/util/oldTests/PokerUtilsTest.java
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.pokergame.util.util;
+package org.pokergame.util.oldTests;
 
 import org.pokergame.Card;
 import org.pokergame.util.PokerUtils;
