ffb4ce4889c28f085f8a697c6932c57f3a254be3
Filip Nordqvist <114819894+Dautabossy@users.noreply.github.com>
2024-02-12 18:38:08 +0100

    #62
    Started testing the new code

--- src/test/java/org/pokergame/CardTest.java
+++ src/test/java/org/pokergame/CardTest.java
@@ -6,42 +6,76 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
 
-    //char[] Suit_Symbols d,c,h,s
+    //char[] SUIT_SYMBOLS = { 'd', 'c', 'h', 's' };
+    //String[] RANK_SYMBOLS 2,3,4,5,6,7,8,9,T,J,Q,K,A
 
     @Test
     void getSuit() {
-        Card card = new Card(Card.JACK,Card.HEARTS);
-        int expected = 2;
+
+        //BVA
+
+        //High
+        Card card = new Card(Card.QUEEN,Card.SPADES);
+        int expected = 3;
         int cardSuit = card.getSuit();
         assertEquals(expected,cardSuit);
-        card = new Card(Card.FIVE,Card.CLUBS);
-        expected = 1;
-        cardSuit = card.getSuit();
-        assertEquals(expected,cardSuit);
+
+        //Low
         card = new Card(Card.EIGHT,Card.DIAMONDS);
         expected = 0;
         cardSuit = card.getSuit();
         assertEquals(expected,cardSuit);
-        card = new Card(Card.QUEEN,Card.SPADES);
-        expected = 3;
+
+        //Invalid, Behöver fixas
+        /*card = new Card(Card.JACK, 4);
+        String invalid = "Invalid suit";
+        cardSuit = card.getSuit();
+        assertNotEquals(invalid, cardSuit);*/
+
+        card = new Card(Card.JACK,Card.HEARTS);
+        expected = 2;
         cardSuit = card.getSuit();
         assertEquals(expected,cardSuit);
-        card = new Card(Card.THREE,5);
+
+        card = new Card(Card.FIVE,Card.CLUBS);
         expected = 1;
         cardSuit = card.getSuit();
-        assertNotEquals(expected,cardSuit);
+        assertEquals(expected,cardSuit);
+
 
     }
 
     @Test
     void getRank() {
-        Card card = new Card(Card.TEN, Card.HEARTS);
 
-        assertEquals(Card.TEN, card.getRank());
+        //BVA
+
+        //High
+        Card card = new Card(Card.ACE, Card.HEARTS);
+        int expected = 12;
+        int cardRank = card.getRank();
+        assertEquals(expected,cardRank);
+
+        //Low
+        card = new Card(Card.DEUCE, Card.DIAMONDS);
+        expected = 0;
+        cardRank = card.getRank();
+        assertEquals(expected,cardRank);
+
+        //Invalid
+        /*card = new Card(13, 0);
+        String invalid = "Invalid rank";
+        cardRank = card.getRank();
+        assertNotEquals(invalid, cardRank);*/
+
     }
 
     @Test
     void testHashCode() {
+        Card card1 = new Card(Card.ACE, Card.HEARTS);
+        int result = 0;
+
+
     }
 
     @Test
