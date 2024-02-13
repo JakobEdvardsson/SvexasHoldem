e9d989128b29d40fff7976d1fbccd59f2e1afdc2
Filip Nordqvist <114819894+Dautabossy@users.noreply.github.com>
2024-02-13 09:40:46 +0100

    Made tests for Player Class

---
+++ src/test/java/org/pokergame/PlayerTest.java
@@ -0,0 +1,76 @@
+package org.pokergame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getClient() {
+    }
+
+    @Test
+    void resetHand() {
+    }
+
+    @Test
+    void resetBet() {
+    }
+
+    @Test
+    void setCards() {
+    }
+
+    @Test
+    void hasCards() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCash() {
+    }
+
+    @Test
+    void getBet() {
+    }
+
+    @Test
+    void setBet() {
+    }
+
+    @Test
+    void getAction() {
+    }
+
+    @Test
+    void setAction() {
+    }
+
+    @Test
+    void isAllIn() {
+    }
+
+    @Test
+    void getCards() {
+    }
+
+    @Test
+    void postSmallBlind() {
+    }
+
+    @Test
+    void postBigBlind() {
+    }
+
+    @Test
+    void payCash() {
+    }
+
+    @Test
+    void win() {
+    }
+}
