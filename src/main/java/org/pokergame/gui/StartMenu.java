1b0f2beff90b593e47299a77d25ce936c5c61bca
Dautabossy <114819894+Dautabossy@users.noreply.github.com>
2024-02-15 21:28:31 +0100

    Look the GUI code over

--- src/main/java/org/pokergame/gui/StartMenu.java
+++ src/main/java/org/pokergame/gui/StartMenu.java
@@ -40,3 +40,36 @@ public class StartMenu extends JFrame {
         setVisible(true);
     }
 }
+/*
+public static final Color POKER_GREEN = new Color(0,153,0);
+JFrame frame;
+        JButton button, button1, button2;
+JLabel label;
+
+public StartMenu() {
+    frame = new JFrame("Start menu");
+    JLabel titleLabel = new JLabel("Start Menu");
+    titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+    frame.add(titleLabel);
+
+    label = new JLabel("KING PÅ SWING!!");
+
+
+    button = new JButton("New game");
+    button1 = new JButton("Play online");
+    button2 = new JButton("Tutorial");
+
+    JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+    panel.add(button, BorderLayout.CENTER);
+    panel.add(button1, BorderLayout.EAST);
+    panel.add(button2, BorderLayout.WEST);
+    panel.add(label);
+    panel.setBackground(POKER_GREEN);
+
+
+    frame.add(panel);
+    frame.setSize(700, 500);
+    frame.setVisible(true);
+
+ */
