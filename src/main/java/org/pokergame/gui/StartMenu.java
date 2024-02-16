9b1875673d321d644cdc77ce5a8947c467b333e7
Dautabossy <114819894+Dautabossy@users.noreply.github.com>
2024-02-16 17:36:52 +0100

    #44
    Made menu with buttons and actionlisteners

--- src/main/java/org/pokergame/gui/StartMenu.java
+++ src/main/java/org/pokergame/gui/StartMenu.java
@@ -14,62 +14,39 @@ public class StartMenu extends JFrame {
         setTitle("Start Menu");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        Label label = new Label("TeachMe poker");
+        label.setBounds(260, 100, 220, 80);
+        label.setFont(new Font("Arial", Font.BOLD, 28));
 
         frame = new JFrame("Start menu");
+        frame.add(label);
 
         button = new JButton("New game");
         button.setBounds(70, 200, 150, 50);
+        button.addChangeListener(
+                e -> System.out.println("New game button pressed")
+        );
         frame.add(button);
         button1 = new JButton("Play online");
         button1.setBounds(265, 200, 150, 50);
+        button1.addChangeListener(
+                e
+                -> System.out.println("Play online button pressed")
+        );
         frame.add(button1);
         button2 = new JButton("Tutorial");
         button2.setBounds(460, 200, 150, 50);
+        button2.addChangeListener(
+                e
+                -> System.out.println("Tutorial button pressed")
+        );
         frame.add(button2);
 
-
+        frame.getContentPane().setBackground(POKER_GREEN);
         frame.setSize(700, 500);
         frame.setLayout(null);
         frame.setResizable(false);
         frame.setVisible(true);
-
-        JPanel panel = new JPanel();
-        panel.setBackground(POKER_GREEN);
-        frame.add(panel);
-
-        /*GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 10, 10); //Padding
-
-        JLabel titleLabel = new JLabel("Start Menu");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 4; //Left/right Padding
-        add(titleLabel, gbc);
-
-
-
-
-
-        String[] buttonNames = {"Button1", "Button2", "Button3", "Button4"};
-        gbc.gridwidth = 1;
-        gbc.gridy = 1;
-
-        for (int i = 0; i < buttonNames.length; i++) {
-            JButton button = new JButton(buttonNames[i]);
-            gbc.gridx = i;
-            add(button, gbc);
-        }
-
-
-        JButton button1 = new JButton("Test1");
-        gbc.gridy = 2;
-        add(button1, gbc);
-
-
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);*/
     }
 }
 
