b1229e52b66eecdea7ffff3f4220ac54fb2d91ce
Dautabossy <114819894+Dautabossy@users.noreply.github.com>
2024-02-16 17:15:22 +0100

    Look the GUI code over

--- src/main/java/org/pokergame/gui/StartMenu.java
+++ src/main/java/org/pokergame/gui/StartMenu.java
@@ -4,12 +4,40 @@ import javax.swing.*;
 import java.awt.*;
 
 public class StartMenu extends JFrame {
+
+    public static final Color POKER_GREEN = new Color(0,153,0);
+    JFrame frame;
+    JButton button, button1, button2;
+    JLabel label;
+
     public StartMenu() {
         setTitle("Start Menu");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridBagLayout());
 
-        GridBagConstraints gbc = new GridBagConstraints();
+
+        frame = new JFrame("Start menu");
+
+        button = new JButton("New game");
+        button.setBounds(70, 200, 150, 50);
+        frame.add(button);
+        button1 = new JButton("Play online");
+        button1.setBounds(265, 200, 150, 50);
+        frame.add(button1);
+        button2 = new JButton("Tutorial");
+        button2.setBounds(460, 200, 150, 50);
+        frame.add(button2);
+
+
+        frame.setSize(700, 500);
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(POKER_GREEN);
+        frame.add(panel);
+
+        /*GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10); //Padding
 
         JLabel titleLabel = new JLabel("Start Menu");
@@ -18,6 +46,10 @@ public class StartMenu extends JFrame {
         gbc.gridy = 0;
         gbc.gridwidth = 4; //Left/right Padding
         add(titleLabel, gbc);
+
+
+
+
 
         String[] buttonNames = {"Button1", "Button2", "Button3", "Button4"};
         gbc.gridwidth = 1;
@@ -29,47 +61,19 @@ public class StartMenu extends JFrame {
             add(button, gbc);
         }
 
-        /*
+
         JButton button1 = new JButton("Test1");
         gbc.gridy = 2;
         add(button1, gbc);
-         */
+
 
         pack();
         setLocationRelativeTo(null);
-        setVisible(true);
+        setVisible(true);*/
     }
 }
-/*
-public static final Color POKER_GREEN = new Color(0,153,0);
-JFrame frame;
-        JButton button, button1, button2;
-JLabel label;
-
-public StartMenu() {
-    frame = new JFrame("Start menu");
-    JLabel titleLabel = new JLabel("Start Menu");
-    titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
-    frame.add(titleLabel);
-
-    label = new JLabel("KING PÅ SWING!!");
 
 
-    button = new JButton("New game");
-    button1 = new JButton("Play online");
-    button2 = new JButton("Tutorial");
-
-    JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-
-    panel.add(button, BorderLayout.CENTER);
-    panel.add(button1, BorderLayout.EAST);
-    panel.add(button2, BorderLayout.WEST);
-    panel.add(label);
-    panel.setBackground(POKER_GREEN);
 
 
-    frame.add(panel);
-    frame.setSize(700, 500);
-    frame.setVisible(true);
 
- */
