0ff3bf6992bdf53208ccdc69297ff4335d545f20
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-03-05 08:43:44 +0100

    Update ServerControllerTest.java

--- src/test/java/org/pokergame/server/ServerControllerTest.java
+++ src/test/java/org/pokergame/server/ServerControllerTest.java
@@ -10,12 +10,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ServerControllerTest {
     private ServerController serverController;
+    String name = "UnitTester";
+    ClientHandler handler;
 
 
     @BeforeEach
     void setUp() {
+        serverController.setServerController(null);
         serverController = ServerController.getInstance();
+        handler = new ClientHandler(null, null);
+        serverController.registerClient(name, handler);
     }
+
     @Test
     void getInstance() {
         ServerController servercontroller1 = ServerController.getInstance();
@@ -34,7 +40,6 @@ class ServerControllerTest {
 
     @Test
     void joinLobby() {
-        ClientHandler handler = new ClientHandler(null, null);
         Lobby lobby = serverController.createLobby();
         serverController.joinLobby(handler, lobby.getLobbyIndex());
         ArrayList<Player> players = lobby.getPlayers();
@@ -52,7 +57,6 @@ class ServerControllerTest {
     @Test
     void leaveLobby() {
         // add a player to the lobby and make sure they are there
-        ClientHandler handler = new ClientHandler(null, null);
         Lobby lobby = serverController.createLobby();
         serverController.joinLobby(handler, lobby.getLobbyIndex());
         ArrayList<Player> players = lobby.getPlayers();
@@ -83,9 +87,69 @@ class ServerControllerTest {
 
     @Test
     void registerClient() {
+        // try to register a client with the same name
+        ClientHandler handler2 = new ClientHandler(null, null);
+        boolean registered = serverController.registerClient(name, handler2);
+        assertFalse(registered);
+
+        // try to register a client with a different name
+        String name2 = "UnitTester2";
+        ClientHandler handler3 = new ClientHandler(null, null);
+        registered = serverController.registerClient(name2, handler3);
+        assertTrue(registered);
     }
 
     @Test
     void disconnectClient() {
+        // add a player to the lobby and make sure they are there
+        Lobby lobby = serverController.createLobby();
+        serverController.joinLobby(handler, lobby.getLobbyIndex());
+        ArrayList<Player> players = lobby.getPlayers();
+
+        boolean found = false;
+        for (Player player : players) {
+            if (player.getClient() == handler) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+
+        // remove the player from the lobby and make sure they are gone
+        serverController.disconnectClient(handler);
+
+        players = lobby.getPlayers();
+
+        boolean isGone = true;
+        for (Player player : players) {
+            if (player.getClient() == handler) {
+                isGone = false;
+                break;
+            }
+        }
+        assertTrue(isGone);
+        assertFalse(serverController.getConnectedClients().containsKey(handler));
+    }
+
+    @Test
+    void getLobbiesAsString() {
+        Lobby lobby = serverController.createLobby();
+
+        try {
+             serverController.joinLobby(handler, lobby.getLobbyIndex());
+        }catch (Exception e){
+            //e.printStackTrace();
+        }
+
+        String[][] lobbies = serverController.getLobbiesAsString();
+
+        boolean found = false;
+        for (String[] lobbyInfo : lobbies) {
+            if (lobbyInfo[0] != null && lobbyInfo[0].equals(name)) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
     }
 }
