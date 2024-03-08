0acf7161c1049466233c062b1d768bce9351dadf
Jakob Edvardsson <jakob.edvardsson@outlook.com>
2024-03-08 16:42:38 +0100

    Test for ClientInput and refactor for making testing easier

--- src/main/java/org/pokergame/client/ClientInputX.java
+++ src/main/java/org/pokergame/client/ClientInputX.java
@@ -22,93 +22,111 @@ public class ClientInputX extends Thread {
     private ObjectInputStream in;
     private ClientController clientController;
     private OnlineMain onlineMain;
+    public Object incomingMessage;
 
 
     public ClientInputX(Socket socket, ClientController clientController) {
         this.socket = socket;
         this.clientController = clientController;
-    }
+   }
 
     @Override
     public void run() {
+        createInputStream();
+
+        while (true) {
+            getInput();
+        }
+    }
+
+    public void getInput() {
+        incomingMessage = recieveMessage();
+
+        if (incomingMessage instanceof String) {
+            System.out.println(incomingMessage);
+        }
+
+        // Lobby information
+        if (incomingMessage instanceof String[][]) {
+            System.out.println(((String[][]) incomingMessage).length);
+
+            for (int i = 0; i < ((String[][]) incomingMessage).length; i++) {
+                for (int j = 0; j < ((String[][]) incomingMessage)[i].length; j++) {
+                    System.out.println(((String[][]) incomingMessage)[i][j]);
+                }
+            }
+
+            clientController.setLobbyInfo((String[][]) incomingMessage);
+        }
+
+        if (incomingMessage instanceof StartGame) {
+            if (onlineMain == null) {
+                onlineMain = new OnlineMain(clientController.getUsername(), clientController);
+            }
+        }
+
+        if (incomingMessage instanceof JoinedTable) {
+            if (onlineMain != null) {
+
+                TableType type = ((JoinedTable) incomingMessage).type();
+                BigDecimal bigBlind = ((JoinedTable) incomingMessage).bigBlind();
+                List<String> players = ((JoinedTable) incomingMessage).players();
+
+                ArrayList<Player> playerObjects = new ArrayList<Player>();
+
+                for (String player : players) {
+                    playerObjects.add(new Player(player, new BigDecimal(500), null));
+                }
+
+
+                onlineMain.joinedTable(type, bigBlind, playerObjects);
+            }
+        }
+
+        if (incomingMessage instanceof HandStarted) {
+            onlineMain.handStarted(((HandStarted) incomingMessage).dealer());
+        }
+
+        if (incomingMessage instanceof BoardUpdated) {
+            List<Card> cards = ((BoardUpdated) incomingMessage).cards();
+            BigDecimal bet = ((BoardUpdated) incomingMessage).bet();
+            BigDecimal pot = ((BoardUpdated) incomingMessage).pot();
+            onlineMain.boardUpdated(cards, bet, pot);
+        }
+
+        if (incomingMessage instanceof ActorRotated) {
+            onlineMain.actorRotated(((ActorRotated) incomingMessage).actor());
+        }
+
+        if (incomingMessage instanceof PlayerActed) {
+            onlineMain.playerActed(((PlayerActed) incomingMessage).player());
+        }
+
+        if (incomingMessage instanceof PlayerUpdated) {
+            onlineMain.playerUpdated(((PlayerUpdated) incomingMessage).player());
+        }
+
+        if (incomingMessage instanceof Act) {
+            BigDecimal minBet = ((Act) incomingMessage).minBet();
+            BigDecimal currentBet = ((Act) incomingMessage).currentBet();
+            Set<PlayerAction> allowedActions = ((Act) incomingMessage).allowedActions();
+            PlayerAction action = onlineMain.act(minBet, currentBet, allowedActions);
+            clientController.sendMessage(action);
+        }
+
+        if (incomingMessage instanceof MessageReceived) {
+            onlineMain.messageReceived(((MessageReceived) incomingMessage).message());
+        }
+    }
+
+    private void createInputStream() {
         try {
             in = new ObjectInputStream(socket.getInputStream());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        while (true) {
-            Object incomingMessage = recieveMessage();
-
-            if (incomingMessage instanceof String) {
-                System.out.println(incomingMessage);
-            }
-
-            // Lobby information
-            if (incomingMessage instanceof String[][]) {
-                clientController.setLobbyInfo((String[][]) incomingMessage);
-            }
-
-            if (incomingMessage instanceof StartGame) {
-                if (onlineMain == null) {
-                    onlineMain = new OnlineMain(clientController.getUsername(), clientController);
-                }
-            }
-
-            if (incomingMessage instanceof JoinedTable) {
-                if (onlineMain != null) {
-
-                    TableType type = ((JoinedTable) incomingMessage).type();
-                    BigDecimal bigBlind = ((JoinedTable) incomingMessage).bigBlind();
-                    List<String> players = ((JoinedTable) incomingMessage).players();
-
-                    ArrayList<Player> playerObjects = new ArrayList<Player>();
-
-                    for (String player : players) {
-                        playerObjects.add(new Player(player, new BigDecimal(500), null));
-                    }
-
-                    onlineMain.joinedTable(type, bigBlind, playerObjects);
-                }
-            }
-
-            if (incomingMessage instanceof HandStarted) {
-                onlineMain.handStarted(((HandStarted) incomingMessage).dealer());
-            }
-
-            if (incomingMessage instanceof BoardUpdated) {
-                List<Card> cards = ((BoardUpdated) incomingMessage).cards();
-                BigDecimal bet = ((BoardUpdated) incomingMessage).bet();
-                BigDecimal pot = ((BoardUpdated) incomingMessage).pot();
-                onlineMain.boardUpdated(cards, bet, pot);
-            }
-
-            if (incomingMessage instanceof ActorRotated) {
-                onlineMain.actorRotated(((ActorRotated) incomingMessage).actor());
-            }
-
-            if (incomingMessage instanceof PlayerActed) {
-                onlineMain.playerActed(((PlayerActed) incomingMessage).player());
-            }
-
-            if (incomingMessage instanceof PlayerUpdated) {
-                onlineMain.playerUpdated(((PlayerUpdated) incomingMessage).player());
-            }
-
-            if (incomingMessage instanceof Act) {
-                BigDecimal minBet = ((Act) incomingMessage).minBet();
-                BigDecimal currentBet = ((Act) incomingMessage).currentBet();
-                Set<PlayerAction> allowedActions = ((Act) incomingMessage).allowedActions();
-                PlayerAction action = onlineMain.act(minBet, currentBet, allowedActions);
-                clientController.sendMessage(action);
-            }
-
-            if (incomingMessage instanceof MessageReceived) {
-                onlineMain.messageReceived(((MessageReceived) incomingMessage).message());
-            }
-
-        }
     }
+
     public synchronized Object recieveMessage(){
         Object incomingObject = null;
         try {
@@ -117,11 +135,15 @@ public class ClientInputX extends Thread {
             throw new RuntimeException(e);
         }
 
-        // Todo - might need to make this async
-        //clientController.inputObject(incomingObject);
-
         return incomingObject;
     }
 
+    public void setOnlineMain(OnlineMain onlineMain) {
+        this.onlineMain = onlineMain;
+    }
+
+    public OnlineMain getOnlineMain() {
+        return onlineMain;
+    }
 
 }
